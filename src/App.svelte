4b7401717162f066c424950054440ba1437d5fc8
Chii Yuen <chii.yuen@hotmail.com>
2024-01-14 03:02:09 +0800

    Complete App.svelte

--- src/App.svelte
+++ src/App.svelte
@@ -1,8 +1,22 @@
 <script lang="ts">
+  import DrumMachine from "./lib/DrumMachine.svelte";
+  import Footer from "./lib/Footer.svelte";
+  import { playClip } from "./lib/DrumPad.svelte";
 </script>
 
 <main>
+  <DrumMachine />
 </main>
+<Footer />
+<svelte:window on:keydown={(e) => playClip(e.key.toUpperCase())} />
 
 <style>
+  @import url("https://fonts.googleapis.com/css2?family=Abel&display=swap");
+
+  main {
+    flex: 1;
+    display: grid;
+    place-items: center;
+    font-family: "Abel", sans-serif;
+  }
 </style>
